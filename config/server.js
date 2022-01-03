48c72942806630d680d186a60905bd45987acc47
psenne <psenne@mjparker.com>
2022-01-02 21:48:19 -0500

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "102bc09080fc24a3376a50e31dac30fe"),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ad1eec3f-8f90-4787-a203-c75aaa46ef43'
+};
